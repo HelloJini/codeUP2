a62dfe8cc896a287602b3798be81af56ce4a92ea
LAPTOP-JMJ6A32F\pc <jiny7157502@naver.com>
2021-06-21 19:59:36 +0900

    코드업 기초100제 6069번

---
+++ 6069.py
@@ -0,0 +1,14 @@
+evaluation = input()
+if(evaluation == "A"):
+    print("best!!!")
+else:
+    if(evaluation == "B"):
+        print("good!!")
+    else:
+        if(evaluation == "C"):
+            print("run!")
+        else:
+            if(evaluation == "D"):
+                print("slowly~")
+            else:
+                print("what?")
